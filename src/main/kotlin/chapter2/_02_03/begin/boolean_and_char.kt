8d66de84d0fd4cae52e2c8f141480986de7f5bd2
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-08-21 19:22:19 +0300

    Chapter 2: data types

--- src/main/kotlin/chapter2/_02_03/begin/boolean_and_char.kt
+++ src/main/kotlin/chapter2/_02_03/begin/boolean_and_char.kt
@@ -10,13 +10,52 @@ fun secondOperand(): Boolean {
     return true;
 }
 
-fun main() {
-//    demoBoolean()
-//    demoChar()
-}
-
 private fun demoBoolean() {
+    var willExercise: Boolean = false
+    val bigNumber = 1_000_000
+    val smallNumber = 2
+
+    println("Will exercise: ${willExercise}")
+    println("Is bigNumber bigger ${bigNumber > smallNumber}")
+
+    if(firstOperand() || secondOperand())
+        println("At least one operand is true")
+
+    if(firstOperand() && secondOperand())
+        println("Both are true")
+
+    println("Will exercise: ${!willExercise}")
 }
 
 private fun demoChar() {
+    val letter: Char = 'A'
+    val tab = '\t'
+    val infinity = '\u221E'
+
+    println("Letter = $letter");
+    println("Tabbed $tab over");
+    println("Infinity = $infinity");
+
+    val lineFeed = 10.toChar()
+    println("line feed = $lineFeed next line")
 }
+
+fun main() {
+    demoBoolean()
+    println(" ========================== ")
+    demoChar()
+}
+
+//Will exercise: false
+//Is bigNumber bigger true
+//firstOperand
+//secondOperand
+//At least one operand is true
+//firstOperand
+//Will exercise: true
+// ==========================
+//Letter = A
+//Tabbed 	 over
+//Infinity = ∞
+//line feed =
+// next line
--- src/main/kotlin/chapter2/_02_04/begin/strings.kt
+++ src/main/kotlin/chapter2/_02_04/begin/strings.kt
@@ -1,4 +1,48 @@
 package chapter2._02_04.begin
 
 fun main() {
+    val greeting = "Hello there"
+    val owe = 50
+
+    val janet = "I owe Janet \$$owe dollars"
+    println(janet)
+
+    val infinity = "The infinity symbol is \u221E"
+    println(infinity)
+
+    println("[$greeting] is ${greeting.length} characters long")
+
+    var bad = greeting.get(2)
+    val letter = greeting[1]
+    println("Bad: $bad")
+    println("Letter: $letter")
+
+    val compare1 = "beta"
+    val compare2 = "alpha"
+
+    println("${compare1.compareTo(compare2)}")
+
+    val sub = greeting.subSequence(6, 9)
+    println("sub = $sub")
+
+    for (single in greeting)
+        print("$single ")
+
+    val myString = """
+        Kotlin is fun
+        Kotlin is pragmatic
+    """
+
+    println(myString)
+
+    //I owe Janet $50 dollars
+    //The infinity symbol is ∞
+    //[Hello there] is 11 characters long
+    //Bad: l
+    //Letter: e
+    //1
+    //sub = the
+    //H e l l o   t h e r e
+    //        Kotlin is fun
+    //        Kotlin is pragmatic
 }
