7919f991b87cde1a0335e5b16b14045b6e437851
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-08-23 14:12:06 +0300

    Challenge if to when

--- src/main/kotlin/chapter3/_03_06/begin/challenge_if_to_when.kt
+++ src/main/kotlin/chapter3/_03_06/begin/challenge_if_to_when.kt
@@ -6,7 +6,7 @@ fun main() {
     // TODO: replace this if with a when
     // bonus points for using ranges!
 
-    val cardLevel: String = if (cardPoints >= 0 && cardPoints < 1000) {
+    var cardLevel: String = if (cardPoints >= 0 && cardPoints < 1000) {
         "pearl"
     } else if (cardPoints >= 1000 && cardPoints < 5_000) {
         "silver"
@@ -15,7 +15,17 @@ fun main() {
     } else {
         "platinum"
     }
+    println("Card Level from if : ${cardLevel}") // Card Level from if : gold
+
+    cardLevel = when(cardPoints) {
+        in 0..999 -> "pearl"
+        in 1000..4999 -> "silver"
+        in 5000..9999 -> "gold"
+        else -> "platinum"
+    }
+    println("Card Level from when: ${cardLevel}") // Card Level from when: gold
 
     val plural = if(cardPoints > 1 || cardPoints == 0) "s" else ""
     println("You have $cardPoints point$plural and are at the $cardLevel level.")
+    //You have 7000 points and are at the gold level.
 }
