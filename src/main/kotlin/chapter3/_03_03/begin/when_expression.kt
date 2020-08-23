d3bd6701186089b67c786133d553727740aa497a
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-08-23 13:33:01 +0300

    when expression

--- src/main/kotlin/chapter3/_03_03/begin/when_expression.kt
+++ src/main/kotlin/chapter3/_03_03/begin/when_expression.kt
@@ -5,4 +5,26 @@ enum class Suit {
 }
 
 fun main() {
+    val x = 3
+    when(x) {
+        1,2 -> println("One or two")
+        3 -> println("Exactly three")
+        4,5 -> println("Four or five")
+    } // Exactly three
+
+    val card = Suit.Spade
+    val y = when(card) {
+        Suit.Spade, Suit.Club -> println("Black card") // Black card
+        Suit.Diamond, Suit.Heart -> println("Red card")
+    }
+    println(y) // kotlin.Unit
+
+    val name = "Troy"
+    val lastName = when(name) {
+        "Hattan" -> "Shobokshi"
+        "Todd" -> "Miller"
+        "Troy" -> "Miles"
+        else -> "Unknown"
+    }
+    println("$name $lastName") // Troy Miles
 }
