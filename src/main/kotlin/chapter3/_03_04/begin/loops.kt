bbeaf58d61acf0734311d6d41707bf29362ded19
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-08-23 13:43:23 +0300

    Loops

--- src/main/kotlin/chapter3/_03_04/begin/loops.kt
+++ src/main/kotlin/chapter3/_03_04/begin/loops.kt
@@ -1,4 +1,35 @@
 package chapter3._03_04.begin
 
 fun main() {
+
+    for (i in 1..10)
+        println("i = $i")
+
+    val students = listOf("Janet", "Daisy", "Veronica", "Fernanda")
+    for (student in students)
+        println("Current student: $student")
+
+    for ((index, student) in students.withIndex())
+        println("Student with index ${index + 1} is $student")
+
+    println("while")
+    var count = 5
+    while (count > 0) {
+        println(count)
+        count--
+    }
+
+    println("do while")
+    do {
+        println(count)
+        count++
+    } while (count < 5)
+
+    println("break")
+    while (count > 0) {
+        if (count % 2 == 0) break
+        println(count)
+        count++
+    }
+
 }
