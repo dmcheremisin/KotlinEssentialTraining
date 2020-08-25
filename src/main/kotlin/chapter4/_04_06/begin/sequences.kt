be59083518cdbecb4756ef05d24d676525907cc2
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-08-25 22:43:26 +0300

    sequences

--- src/main/kotlin/chapter4/_04_06/begin/sequences.kt
+++ src/main/kotlin/chapter4/_04_06/begin/sequences.kt
@@ -1,10 +1,14 @@
 package chapter4._04_06.begin
 
 fun main() {
-    val result = (100 until 200)
+    val result = (100 until 200).asSequence()
         .map { println("doubling $it"); it * 2 }
         .filter { println("filtering $it"); it % 3 == 0 }
         .first()
     println("results = $result")
     println()
+
+    val seq = sequenceOf(1, 2, 3, 4, 5, 6).filter { it % 3 == 0 }
+    println(seq)//kotlin.sequences.FilteringSequence@1efbd816
+    println(seq.toList()) // [3, 6]
 }
