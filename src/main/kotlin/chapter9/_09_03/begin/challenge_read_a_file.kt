3e8b34816621b4194f732c8715e6eb71c19b4336
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-08-26 23:45:45 +0300

    Coroutines challenge

--- src/main/kotlin/chapter9/_09_03/begin/challenge_read_a_file.kt
+++ src/main/kotlin/chapter9/_09_03/begin/challenge_read_a_file.kt
@@ -1,8 +1,23 @@
 package chapter9._09_03.begin
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.io.File
 
+fun asyncReader() = runBlocking {
+    val fileLines = async {readFile()}
+    fileLines.await().forEach({println(it)})
+}
+
+fun readFile (): List<String> {
+    val file = File("./resources/text.txt")
+    return file.readLines()
+}
+
 fun main() {
+    asyncReader()
+    //1 alpha
+    //2 beta
+    //3 gamma
+    //4 delta
+    //5 epsilon
 }
