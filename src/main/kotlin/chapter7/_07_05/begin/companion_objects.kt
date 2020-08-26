68fff31bdc26e01ca2b89634ec50baee57aa0c05
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-08-26 19:08:15 +0300

    Kotlin companion object

--- src/main/kotlin/chapter7/_07_05/begin/companion_objects.kt
+++ src/main/kotlin/chapter7/_07_05/begin/companion_objects.kt
@@ -4,7 +4,25 @@ class CantCreate constructor(val message: String) {
     fun showMessage() {
         println(message)
     }
+
+    companion object {
+        const val LIGHT_SPEED = 299_792_458
+        fun factory(msg: String): CantCreate {
+            return CantCreate(msg)
+        }
+
+        fun sillyPrint(self: CantCreate) {
+            println("Accessing ${self.message}")
+        }
+    }
 }
 
 fun main() {
+    val c1 = CantCreate.factory("Hello, there")
+    c1.showMessage()
+    CantCreate.sillyPrint(c1)
+    println("The speed of light is ${CantCreate.LIGHT_SPEED}")
+    //Hello, there
+    //Accessing Hello, there
+    //The speed of light is 299792458
 }
