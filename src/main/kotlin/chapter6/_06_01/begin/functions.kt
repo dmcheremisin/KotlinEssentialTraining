e96c2cd0c11e6f15b4a8307773b6e3029a476bca
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-08-26 12:04:15 +0300

    Functions

--- src/main/kotlin/chapter6/_06_01/begin/functions.kt
+++ src/main/kotlin/chapter6/_06_01/begin/functions.kt
@@ -1,4 +1,20 @@
 package chapter6._06_01.begin
 
 fun main() {
+    println(greeter("Troy")) // Hello Troy
+    println(greeter2("Troy"))// Hello Troy
+    println(volume(1, 2, 3)) // 6
+    println(volumeWithDefault(1, 2)) // 20
+    println(volumeX(width = 2, height =  3)) // 60
 }
+
+fun greeter(name: String): String {
+    return "Hello $name"
+}
+
+fun greeter2(name: String) = "Hello $name"
+
+fun volume(length: Int, width: Int, height: Int) = length * width * height
+fun volumeWithDefault(length: Int, width: Int, height: Int = 10) = length * width * height
+
+fun volumeX(length: Int = 10, width: Int, height: Int) = length * width * height
