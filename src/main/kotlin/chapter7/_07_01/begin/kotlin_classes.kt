96332af9e4c732bde135c75e40712a8d4c9b70e5
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-08-26 18:19:21 +0300

    Kotlin classes

--- src/main/kotlin/chapter7/_07_01/begin/kotlin_classes.kt
+++ src/main/kotlin/chapter7/_07_01/begin/kotlin_classes.kt
@@ -1,4 +1,39 @@
 package chapter7._07_01.begin
 
+class PlainShort {}
+
+class ShortClass
+
+class WithConstructor constructor(name: String, age: Int){}
+
+class WithoutConstructor (name: String, age: Int) {}
+
+class MandatoryConstructor internal constructor(name: String, age: Int) {}
+
+class CourseProperty(courseTitle: String) {
+    val title = courseTitle
+}
+
+class Person (val firstName: String, lastName: String, var age: Int) {}
+
+class Course(courseTitle: String){
+    private val title = courseTitle
+    internal var description = ""
+    get() {
+        return if(field.isNullOrBlank()) "no description" else "$field!"
+    }
+    fun show() {
+        println(title)
+    }
+
+    override fun toString(): String {
+        return "$title - $description"
+    }
+}
+
 fun main() {
+    val course = Course("Kotlin essential training")
+    course.description = "secrets you want to know"
+    course.show() // Kotlin essential training
+    println(course) // Kotlin essential training - secrets you want to know!
 }
