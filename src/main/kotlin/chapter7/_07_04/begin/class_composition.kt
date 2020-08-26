51807eeb4ad30b59e1c68e767585febf404d74d0
Dmitrii Cheremisin <Dmitrii_Cheremisin@epam.com>
2020-08-26 18:57:25 +0300

    Kotlin composition of classes

--- src/main/kotlin/chapter7/_07_04/begin/class_composition.kt
+++ src/main/kotlin/chapter7/_07_04/begin/class_composition.kt
@@ -1,1 +1,63 @@
 package chapter7._07_04.begin
+
+interface IWorker {
+    val jobTitle: String
+    fun trackHours(hours: Double)
+    fun doWork()
+}
+
+interface IBeing {
+    val firstName: String
+    fun getName(): String
+}
+
+class Worker(override val jobTitle: String) : IWorker {
+    private var totalHours = 0.0
+
+    override fun trackHours(hours: Double) {
+        totalHours += hours
+    }
+
+    override fun doWork() {
+        println("Doing ${this.jobTitle} work.")
+    }
+}
+
+class Being(override val firstName: String) : IBeing {
+    override fun getName(): String {
+        return firstName
+    }
+}
+
+interface IPayment {
+    fun calcPay(): Double
+}
+
+class HourlyPay(val hourlyRate: Double) : IPayment {
+    override fun calcPay(): Double {
+        return hourlyRate
+    }
+}
+
+class SalariedPay(val yearlyRate: Double) : IPayment {
+    override fun calcPay(): Double {
+        return yearlyRate
+    }
+}
+
+class Employee(worker: IWorker, being: IBeing, pay: IPayment) :
+    IWorker by worker, IBeing by being, IPayment by pay
+
+fun main() {
+    val hourly = Employee(Worker("bit wrangler"), Being("abel"), HourlyPay(42.42))
+    println("My name is ${hourly.getName()}")
+    hourly.doWork()
+    //My name is abel
+    //Doing bit wrangler work.
+
+    val salaried = Employee(Worker("program manager"), Being("beth"), SalariedPay(100_000.0))
+    println("My name is ${salaried.getName()}")
+    salaried.doWork()
+    //My name is beth
+    //Doing program manager work.
+}
